22ff17c46f4b209904357873847fa343f8310d90
Akash T B <akashtb21@gmail.com>
2025-03-09 23:25:05 +0530

    database connection has been maded successfully

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import connect from "./db.js"
+
 
 const app = express()
 dotenv.config()
@@ -10,6 +12,8 @@ app.get('/',(req,res)=>{
     res.send("Hello, World!");
 })
 
+
+connect();
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
