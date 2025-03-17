17712933ddaed6559d5d00858ed8fc01638d011a
jeff-antony <antonyjefin03@gmail.com>
2025-03-17 18:52:34 +0530

    home chart designs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,54 @@
-import './App.css'
+
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router"
 import LandingPage from './Pages/LandingPage/LandingPage'
 import Login from './Pages/LoginPages/PrincipalLoginPage'
 import StudentRegister from './Pages/StudentRegister/StudentRegister'
-
+import "./styles/global.scss";
+import Home from "./pages/home/Home"
+import User from "./pages/users/User"
+import Products from "./pages/products/Products"
+import Navbar from "./components/navbar/Navbar"
+import Menu from "./components/menu/Menu"
+import Footer from "./components/footer/Footer"
 function App() {
-
+  const Layout = () =>{
+    return(
+      <div className="main"> 
+      <Navbar/>
+      <div className="container">
+        <div className="menuContainer">
+          <Menu/>
+        </div>
+        <div className="contentContainer">
+          <Outlet/>
+        </div>
+      </div>
+      <Footer/>
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>, 
+      children : [
+        { path: "/", element: <Home /> },
+        { path: "/users", element: <User /> },
+        { path: "/products", element: <Products /> }
+        
+      ]
+    },
+    {
+    path: "/login",
+    element: <Login/>, 
+    }
+  ])
   return (
     <>
-    <Login/>
+    {/* <Login/> */}
     {/* <StudentRegister/> */}
     {/* <LandingPage/> */}
+    <RouterProvider router={router}/>
     </>
   )
 }
