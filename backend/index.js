0bf708c54ad96c5178fed799bd01b23fb299c710
Akash T B <akashtb21@gmail.com>
2025-03-17 16:52:25 +0530

    custom error handling methods are implemented and now it have to tested

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,17 @@ app.use("/hod",hodRoutes)
 app.use("/student",studentRoutes)
 
 
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack:err.stack
+    })
+})
+
 connect();
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on ${process.env.PORT}`)
