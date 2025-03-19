dbae98c291718066aa9adc091ca80f259647ca46
Akash T B <akashtb21@gmail.com>
2025-03-19 12:04:18 +0530

    request letter models are created

---
+++ backend/models/requestLetter.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const requestLetterSchema = new mongoose.Schema(
+  {
+    fromUid: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    fromRole: {
+      type: String,
+      enum: ["student", "tutor", "hod"], 
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    messageBody: {
+      type: String,
+      required: true,
+    },
+    toUids: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+        },
+        role: {
+          type: String,
+          enum: ["principal", "tutor", "hod"],
+          required: true,
+        },
+      },
+    ],
+    approvedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const RequestLetter = mongoose.model("RequestLetter", requestLetterSchema);
+export default RequestLetter;
