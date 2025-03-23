e550eab9edd1cd7bb2a13f34e47366aea0da533f
Akash T B <akashtb21@gmail.com>
2025-03-23 10:13:28 +0530

    tutor section design is also completed

---
+++ client/src/Pages/users/Tutor/Tutor.jsx
@@ -0,0 +1,75 @@
+import DataTable from "../../../components/dataTable/DataTable";
+import "./hod.scss";
+import { useState } from "react";
+import Add from "../../../components/add/Add";
+import { userRows } from "../../../data";
+import Edit from "../../../Components/Edit/Edit";
+
+const columns = [
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="avatar" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First Name",
+    flex: 1.5, 
+    minWidth: 150, 
+  },
+  {
+    field: "lastName",
+    headerName: "Last Name",
+    flex: 1.5,
+    minWidth: 150,
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    flex: 2.5,
+    minWidth: 200,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    flex: 1.5,
+    minWidth: 150,
+  },
+  {
+    field: "dateOfBirth",
+    headerName: "Date of Birth",
+    flex: 1.5,
+    minWidth: 150,
+  },
+  {
+    field: "departmentName",
+    headerName: "Department",
+    flex: 2,
+    minWidth: 200, 
+  },
+];
+
+
+const Tutor = () => {
+  const [open, setOpen] = useState(false);
+  const [openEdit,setOpenEdit] = useState(false)
+
+  return (
+    <div className="Tutor">
+      <div className="info">
+        <h1>Tutor</h1>
+        <button onClick={() => setOpen(true)}>Add New Tutor</button>
+      </div>
+      <div className="tableContainer">
+        <DataTable slug="Tutor" columns={columns} rows={userRows} setOpenEdit={setOpenEdit} />
+      </div>
+      {open && <Add slug="Tutor" columns={columns} setOpen={setOpen} />}
+      {openEdit && <Edit slug="Tutor" columns={columns} setOpenEdit={setOpenEdit} />}
+    </div>
+  );
+};
+
+export default Tutor;
