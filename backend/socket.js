fd7c92bf5840d89bb31a0c7c2575412a8318b6ab
Akash T B <akashtb21@gmail.com>
2025-08-19 19:28:02 +0530

    add user through socket is completely working

--- backend/socket.js
+++ backend/socket.js
@@ -1,11 +1,26 @@
+let users = [];
+
+const addUser = (userId,socketId)=>{
+  if(!users.some(user=>user.userId === userId)){
+    users.push({ userId, socketId });
+  }
+}
+
+const removeUser = (socketId) => {
+  users = users.filter(user => user.socketId !== socketId);
+}
+
 export const socketHandler = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
 
-  
-
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      console.log("Current Users:", users);
+    })
     socket.on("disconnect", () => {
       console.log("User disconnected:", socket.id);
+      removeUser(socket.id);
     });
 
     socket.emit("welcome", "Welcome! You are connected to the server 🚀");
--- client/src/features/context/SocketContext.jsx
+++ client/src/features/context/SocketContext.jsx
@@ -1,31 +1,36 @@
-import React, { createContext, useEffect, useRef} from "react";
+import React, { createContext, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
+import { selectCurrentRole, selectCurrentUser } from "../redux/auth/AuthSlice";
+import { useSelector } from "react-redux";
 
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-    const socketRef = useRef(null);
+  const socketRef = useRef(null);
+  const userId = useSelector(selectCurrentUser);
+  const userType = useSelector(selectCurrentRole);
 
-    useEffect(() => {
-        socketRef.current = io("http://localhost:3007", {
-        });
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3007");
 
-        socketRef.current.on("connect", () => {
-            console.log("Socket connected:", socketRef.current.id);
-        });
+    socketRef.current.on("connect", () => {
+      console.log("Socket connected:", socketRef.current.id);
 
-        socketRef.current.on("disconnect", () => {
-            console.log("Socket disconnected");
-        });
+      socketRef.current.emit("addUser", { userId, userType });
+    });
 
-        return () => {
-            socketRef.current.disconnect();
-        };
-    }, []);
+    socketRef.current.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
 
-    return (
-        <SocketContext.Provider value={{ socket: socketRef.current}}>
-            {children}
-        </SocketContext.Provider>
-    );
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, [userId, userType]);
+
+  return (
+    <SocketContext.Provider value={{ socket: socketRef.current }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
